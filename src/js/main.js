97f126401d4408d88168b841b22e657d193b3711
fengpc <zhiqiang.feng@qq.com>
2016-07-26 14:38:30 +0800

    search 样式ok

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,34 @@
  */
 "use strict";
 require('../style.css');
-require('./index.slide.js');
+// require('./index.slide.js');
 
-$(function() {
+$(function () {
     //变量
     let $dropDown = $('.js-dropdown');
+    let $search = $('#mainSearch');
     //dropdown
-    $dropDown.hover(function() {
+    $dropDown.hover(function () {
         $(this).addClass('open');
-    }, function() {
+    }, function () {
         $(this).removeClass('open');
     });
+    //search
+    $search.find('input').on({
+        focus: function () {
+            $search.addClass('active');
+        },
+        input:function(){
+            if(!$(this).val()){
+                $search.removeClass('active');
+            }
+        },
+        mouseout:function(){
+            if(!$(this).val()){
+                $search.removeClass('active');
+            }
+        }
+    });
 
-});
+    //function END
+})
