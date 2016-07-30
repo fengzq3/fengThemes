2a5d6bb3cee41e1ad763285e34dfa55c513ef6c8
fengpc <zhiqiang.feng@qq.com>
2016-07-30 12:14:20 +0800

    mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,10 @@ require('../style.css');
 
 $(function () {
     //变量
-    let $dropDown = $('.js-dropdown');
-    let $search = $('#mainSearch');
+    let $dropDown = $('.js-dropdown');  //下拉
+    let $search = $('#mainSearch'); //搜索
+    let $mobileNav = $('#mobileNav');   //移动导航
+    let $mobileNavBtn = $('#mobileNavBtn'); //移动导航按钮
     //dropdown
     $dropDown.hover(function () {
         $(this).addClass('open');
@@ -20,15 +22,25 @@ $(function () {
         focus: function () {
             $search.addClass('active');
         },
-        input:function(){
-            if(!$(this).val()){
+        input: function () {
+            if (!$(this).val()) {
                 $search.removeClass('active');
             }
         },
-        mouseout:function(){
-            if(!$(this).val()){
+        mouseout: function () {
+            if (!$(this).val()) {
                 $search.removeClass('active');
             }
+        }
+    });
+    // 移动胶囊导航
+    $mobileNavBtn.on('click', function () {
+        if (!$mobileNav.hasClass('in')) {
+            $(this).addClass('open');
+            $mobileNav.addClass('in');
+        } else {
+            $(this).removeClass('open');
+            $mobileNav.removeClass('in');
         }
     });
 
